f7df329642eb63ca6ec3676235c301d6781e0752
otienojoseph <otienoj.cruz@gmail.com>
2024-01-07 19:20:46 +0300

    Add 0-simply_match_school

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+def match_school(input)
+  regex = /School/
+
+  matches = input.scan(regex)
+
+  unless matches.empty?
+    puts "#{matches.join('')}"
+  end
+end
+
+input_arg = ARGV[0]
+
+if input_arg.nil?
+  puts "Please provide an argument."
+else
+  match_school(input_arg)
+end
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,3 @@
+# Regular Expressions
+
+- 0-simply_match_school - Script that accepts one argument and pass it to a regular expression matching function

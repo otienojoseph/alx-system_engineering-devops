685f032e41049b374fe93f0ea7b6eaa5e75e5047
otienojoseph <otienoj.cruz@gmail.com>
2024-04-20 17:54:27 +0300

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Configure ssh config to use ssh without being asked for password
 
-file { '2-ssh_config': 
-    path => '/home/optimus/alx_cscience/alx-system_engineering-devops/0x0B-ssh/2-ssh_config',
-    ensure => 'file',
+file { '2-ssh_config':
+    ensure  => 'file',
+    path    => '/home/optimus/alx_cscience/alx-system_engineering-devops/0x0B-ssh/2-ssh_config',
     content => "
         Host custom
             HostName 54.160.112.244
             User ubuntu
-            PasswordAuthentication no
             IdentityFile ~/.ssh/school
+            PasswordAuthentication no
     "
     }

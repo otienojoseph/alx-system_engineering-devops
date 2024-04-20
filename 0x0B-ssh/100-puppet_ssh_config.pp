6127a5a0bc9c2fea67e62efe86ec083ff56d72df
otienojoseph <otienoj.cruz@gmail.com>
2024-04-20 18:17:21 +0300

    fix: Add mode

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # Configure ssh config to use ssh without being asked for password
+$config_path = '/home/optimus/alx_cscience/alx-system_engineering-devops/0x0B-ssh/2-ssh_config'
 
 file { '2-ssh_config':
-    ensure  => 'file',
-    path    => '/home/optimus/alx_cscience/alx-system_engineering-devops/0x0B-ssh/2-ssh_config',
+    ensure  => file,
+    path    => $config_path,
+    mode    => '0644',
     content => "
-        Host custom
-            HostName 54.160.112.244
-            User ubuntu
-            IdentityFile ~/.ssh/school
-            PasswordAuthentication no
+    Host custom
+        HostName 54.160.112.244
+        User ubuntu
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
     "
     }
